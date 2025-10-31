f524e78ed2890709d4319f04839d69e96bec0be0
Andrew <huli-gan_94@mail.ru>
2025-10-31 16:44:15 +0300

    Добавлен Footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './index.css'
-import Header from './components/Header/Header'
+import "./index.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-
-
   return (
     <>
-     <div className='min-h-screen flex flex-col'>
-      <Header />
-       <main className='flex-1'>
-           <h1 className='text-4xl font-bold text-gray-800 mb-4'>Hello World</h1>
-       </main>
-     </div>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">Hello World</h1>
+        </main>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
