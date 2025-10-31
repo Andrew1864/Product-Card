34f15c284047d1dfd29bd7833446dc2311b27366
Andrew <huli-gan_94@mail.ru>
2025-10-31 16:26:29 +0300

    Добавлен Header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './index.css'
-
+import Header from './components/Header/Header'
 function App() {
 
 
   return (
     <>
-     <div className=''>
-      <h1 className='text-4xl font-bold text-gray-800 mb-4'>Hello World</h1>
+     <div className='min-h-screen flex flex-col'>
+      <Header />
+       <main className='flex-1'>
+           <h1 className='text-4xl font-bold text-gray-800 mb-4'>Hello World</h1>
+       </main>
      </div>
     </>
   )
