b44ca5178f2bd42a53cf1d6a7add002f36a68d12
Andrew <huli-gan_94@mail.ru>
2025-10-31 15:44:44 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className=''>
+      <h1 className='text-4xl font-bold text-gray-800 mb-4'>Hello World</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
