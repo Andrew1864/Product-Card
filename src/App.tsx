87aefbb80653f7e082e19ac898cf35eb7181251c
Andrew <huli-gan_94@mail.ru>
2025-11-01 14:37:06 +0300

    Добавлены переход на страницу Card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NewsPage from "./news/Page";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">Hello World</h1>
+          <Routes>
+            <Route path="/newsPage" element={<NewsPage />} />
+          </Routes>
         </main>
         <Footer />
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClickNavigate = () => {
+    navigate('/newsPage')
+  }
   return (
     <>
       <header>
@@ -56,12 +63,12 @@ const Header = () => {
                   </a>
                 </li>
                 <li>
-                  <a
-                    href="#"
+                  <button
+                    onClick={handleClickNavigate}
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Product
-                  </a>
+                  </button>
                 </li>
                 <li>
                   <a
