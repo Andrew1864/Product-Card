ab796bbcde5fa34001860aeb1130f6503f5ff371
Andrew <huli-gan_94@mail.ru>
2025-11-02 15:32:55 +0300

    Добавил навигацию в избранные новости.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NewsPage from "./news/page";
+import FavoriteList from "./favoriteList/page";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
@@ -13,6 +14,7 @@ function App() {
           <h1 className="text-4xl font-bold text-gray-800 mb-4">Hello World</h1>
           <Routes>
             <Route path="/newsPage" element={<NewsPage />} />
+             <Route path="/favoriteList" element={<FavoriteList />} />
           </Routes>
         </main>
         <Footer />
