705fbc0eee2a0d0cd698039d4055c685108e469d
Andrew <huli-gan_94@mail.ru>
2025-11-02 14:15:43 +0300

    Поправил страницу с показом  новостей.

--- src/news/page.tsx
+++ src/news/page.tsx
@@ -31,14 +31,7 @@ export default function NewsPage() {
       )}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
         {news.map((article, index) => (
-          <Card
-            key={`${article.url}-${index}`}
-            id={index}
-            title={article.title}
-            description={article.description || "No description available"}
-            img={article.image}
-            isFavorite={false}
-          />
+          <Card key={`${article.url}-${index}`} article={article} />
         ))}
       </div>
       {!loading && news.length === 0 && (
